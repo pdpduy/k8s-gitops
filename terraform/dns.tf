9373705f7de32260cef517709dd079dd6ce974aa
Duy Pham <duy.pham@dinotronic.ch>
2026-09-19 11:07:23 +0700

    update cert issuer

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,15 +1,23 @@
 locals {
-  # Assuming your base Cloudflare zone is "nexusworkspace.cloud"
   dns_records = ["argocd.lab", "grafana.lab", "prometheus.lab"]
 }
 
-resource "cloudflare_record" "ingress_records" {
+resource "cloudflare_dns_record" "ingress_records" {
   for_each = toset(local.dns_records)
   
-  zone_id  = var.cloudflare_zone_id
-  name     = each.key
-  value    = azurerm_public_ip.lb.ip_address
-  type     = "A"
-  proxied  = false # Set to true if you want to use Cloudflare's proxy/CDN
-  ttl      = 120
+  zone_id = var.cloudflare_zone_id
+  name    = each.key
+  content = azurerm_public_ip.lb.ip_address # Replaced 'value' with 'content'
+  type    = "A"
+  proxied = false
+  ttl     = 120
+}
+
+resource "cloudflare_dns_record" "jumpbox" {
+  zone_id = var.cloudflare_zone_id
+  name    = "jumpbox.lab"
+  content = azurerm_public_ip.jumpbox.ip_address # Points to the jumpbox IP
+  type    = "A"
+  proxied = false
+  ttl     = 120
 }
